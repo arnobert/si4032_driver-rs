9d681a10acd3bac39d706752ad44caacf5e20a49
arnobert <arnodaeuper@gmx.de>
2023-03-16 17:03:07 +0100

    Added prototypes

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,29 @@
 #![no_std]
 
 pub fn init() {}
+
+fn write_register(reg: u8, data: u8) {}
+fn read_register(reg: u8) -> u8 {0}
+
+pub fn set_freq() {}
+
+pub fn set_tx_pwr() {}
+
+pub fn write_fifo() {}
+
+
+
+pub fn get_device_status () {}
+
+pub fn set_tx () {}
+
+pub fn set_sync_wrd() {}
+
+pub fn set_tx_header() {}
+
+pub fn set_tx_packet_leng() {}
+
+pub fn set_modulation_mode() {}
+
+
+
