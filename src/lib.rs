a0a3a522221ad4bbe9dba1f1ce907dab5321ee99
arnobert <arnodaeuper@gmx.de>
2023-03-21 19:04:20 +0100

    Some initial work on si4032 driver

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,29 @@
 #![no_std]
 
-pub fn init() {}
+use embedded_hal as hal;
+use embedded_hal::digital::v2::OutputPin;
+use hal::blocking::spi;
+use stm32f1xx_hal::{spi::*};
+
+pub struct Si4032<SPI, CS> {
+    spi: SPI,
+    cs: CS
+}
+
+impl<SPI, CS, E, PinError> Si4032<SPI, CS>
+where
+    SPI: spi::Transfer<u8, Error=E> + spi::Write<u8, Error=E>,
+    CS: OutputPin<Error = PinError>
+{
+    pub fn new(spi:SPI, cs:CS) -> u8 {
+        let mut radio = Si4032  {
+            spi,
+            cs};
+        0
+    }
+}
+
+
 
 fn write_register(reg: u8, data: u8) {}
 fn read_register(reg: u8) -> u8 {0}
